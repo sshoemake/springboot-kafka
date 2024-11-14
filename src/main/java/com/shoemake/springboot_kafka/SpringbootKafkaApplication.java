a7a49694da5d9087ef1c8f67c58eaadab6bff646
shoes <sshoemake@centurylink.com>
2024-11-14 12:22:22 -0700

    daily push

--- src/main/java/com/shoemake/springboot_kafka/SpringbootKafkaApplication.java
+++ src/main/java/com/shoemake/springboot_kafka/SpringbootKafkaApplication.java
@@ -1,7 +1,10 @@
 package com.shoemake.springboot_kafka;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class SpringbootKafkaApplication {
@@ -10,4 +13,10 @@ public class SpringbootKafkaApplication {
 		SpringApplication.run(SpringbootKafkaApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+		return args -> {
+			kafkaTemplate.send("foobar", "hello kafka");
+		};
+	}
 }
